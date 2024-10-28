ec76b4803baf4ebfbe3a1f2f4ab25b82b89150f0
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-28 16:40:53 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -65,7 +65,7 @@ function findById(user_id) {
    */
     return db('users')
     .join('roles', 'users.role_id', 'roles.role_id')
-    .select('user_id', 'username', 'role_name', 'password')
+    .select('user_id', 'username', 'role_name')
     .where('users.user_id', user_id).first()
 }
 
