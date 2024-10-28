60407411e79b76599a24810346a7e570d67f4c3f
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-28 16:06:36 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -63,6 +63,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where('users.user_id', user_id).first()
 }
 
 /**
