6b141f88dc44f9a3c21459aad63e5407f96f389d
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-28 17:15:45 -0500

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,6 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { findBy } = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
   /*
@@ -16,7 +18,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-  next()
+    const token = req.headers.authorization
+    if (!token) {
+      return next({ status: 401, message: 'Token required' })
+    } 
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: "Token invalid" })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
 }
 
 const only = role_name => (req, res, next) => {
@@ -34,7 +47,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,7 +55,18 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-  next()
+  try {
+    const [user] = await findBy({username: req.body.username})
+    if (!user) {
+      next({status: 422, message: 'Invalid credentials'})
+    } else {
+      req.user = user
+      next()
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
 
 
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -48,6 +48,28 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+    const token = buildToken(req.user)
+    res.json({
+      message: `${req.user.username} is back!`,
+      token,
+    })
+  } else {
+    next({ status: 401, message:'Invalid credentials'})
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    role_name: user.role_name,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+
+}
 
 module.exports = router;
